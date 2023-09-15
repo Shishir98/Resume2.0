cc767d9084e4ed851a99e69db8398f7de4823e11
Shishir Saxena <shishir98@live.com>
2023-09-16 01:26:06 +0530

    Created functionality to download resume

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ Parameters:
 Returns:
     TemplateResponse: An HTML response rendered using Jinja2 templates.
 """
-
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.responses import FileResponse
 app = FastAPI()
 
 # Mount the "/static" route to serve static files from the "static" directory
@@ -26,6 +25,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Configure Jinja2Templates to load templates from the "templates" directory
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 async def read_root(request: Request):
@@ -39,3 +39,23 @@ async def read_root(request: Request):
         TemplateResponse: An HTML response rendered using Jinja2 templates.
     """
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/download/resume")
+async def download_resume():
+    """
+    Endpoint to download a resume PDF file.
+
+    Returns:
+        FileResponse: A response containing the PDF file as an attachment.
+    """
+
+    # Check if the uploaded file is a PDF
+    # Replace 'path/to/your.pdf' with the actual path to your PDF file
+    pdf_file_path = r'static/website/documents/Shishir Saxena - Resume.docx.pdf'
+
+    # Provide a custom name for the downloaded file (optional)
+    # You can also use the original filename from the path if needed.
+    custom_file_name = 'resume.pdf'
+    return FileResponse(pdf_file_path, headers={"Content-Disposition": f"attachment; filename={custom_file_name}"})
+
